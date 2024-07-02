624f4db12d61313fb6b9864ab50566f1c2e4289e
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-07-02 19:47:54 -0300

    Episódio 08 - Consultas por Nomes de Métodos

--- projetoempresa/src/main/java/br/com/professorisidro/projetoempresa/dao/DepartamentoDAO.java
+++ projetoempresa/src/main/java/br/com/professorisidro/projetoempresa/dao/DepartamentoDAO.java
@@ -2,7 +2,9 @@ package br.com.professorisidro.projetoempresa.dao;
 
 import br.com.professorisidro.projetoempresa.model.Departamento;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface DepartamentoDAO extends CrudRepository<Departamento, Integer> {
-
+    public List<Departamento> findByOrderByNome();
+    public List<Departamento> findByAndar(int andar);
 }
